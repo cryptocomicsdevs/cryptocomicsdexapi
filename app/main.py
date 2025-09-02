ce15138022815fbdf1809ec3f7a5f32ecfcc328e
ignismeow <saadbeenco@gmail.com>
2025-09-02 20:00:41 +0500

    feat: add CORS middleware to allow cross-origin requests

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.router.pools import router as pools
 import time
 from starlette.requests import Request
 
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
